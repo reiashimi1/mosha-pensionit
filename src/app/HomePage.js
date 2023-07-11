c0014fb4adde1b02cb10f3898ff1e0f31e95d83c
Rei Ashimi <reiashimi1@gmail.com>
2023-07-12 01:44:37 +0200

    Fixed all the logic

---
+++ src/app/HomePage.js
@@ -0,0 +1,130 @@
+import React, {useMemo, useState} from 'react';
+import {
+    Button,
+    Card,
+    CardContent,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Input,
+    Radio,
+    RadioGroup
+} from "@mui/material";
+import moment from "moment/moment";
+
+const HomePage = () => {
+    const [gender, setGender] = useState("male");
+    const [birthday, setBirthday] = useState("");
+    const [displayResults, setDisplayResults] = useState(false);
+    const [finalRetirementDate, setFinalRetirementDate] = useState(null);
+
+    const onSubmit = () => {
+        if (gender === "male") {
+            calculateRetirementYear(65, 0, true)
+        } else {
+            calculateRetirementYear(60, 2)
+        }
+        setDisplayResults(true);
+    }
+
+    const calculateRetirementYear = (baseYears, baseMonths, isMale = false) => {
+        if ((isMale && moment(birthday).get('year') <= 1967) || (!isMale && moment(birthday).get('year') <= 1955)) {
+            const retirementDate = moment(birthday).add(baseYears, 'year').add(baseMonths, 'months').format('YYYY-MM-DD');
+            setFinalRetirementDate(retirementDate);
+            return;
+        }
+        if (isMale) {
+            const monthsToAdd = moment(birthday).get('year') - 1967;
+            if (monthsToAdd > 24) {
+                const retirementDate = moment(birthday).add(67, 'years').format('YYYY-MM-DD');
+                setFinalRetirementDate(retirementDate);
+            } else {
+                const retirementDate = moment(birthday).add(baseYears, 'years').add(monthsToAdd, 'months').format('YYYY-MM-DD');
+                setFinalRetirementDate(retirementDate);
+            }
+        } else {
+            const monthsToAdd = (moment(birthday).get('year') - 1955) * 2;
+            if (monthsToAdd > 82) {
+                const retirementDate = moment(birthday).add(67, 'year').format('YYYY-MM-DD');
+                setFinalRetirementDate(retirementDate);
+            } else {
+                const retirementDate = moment(birthday).add(baseYears, 'years').add(monthsToAdd, 'months').format('YYYY-MM-DD');
+                setFinalRetirementDate(retirementDate);
+            }
+        }
+    }
+
+    const ageOfRetirement = useMemo(() => {
+        if (!!finalRetirementDate) {
+            const age = moment(finalRetirementDate.toString(), 'YYYY-MM-DD');
+            if (age) {
+                const yearsDiff = age.diff(birthday, 'years');
+                age.subtract(yearsDiff, 'years');
+                const monthsDiff = age.diff(birthday, 'months');
+                return yearsDiff + " vjec e " + monthsDiff + " muaj";
+            }
+            return '';
+        }
+    }, [finalRetirementDate])
+
+    const calculateWorkExperience = useMemo(() => {
+        if (!finalRetirementDate) {
+            return '';
+        }
+        let baseYears = 35;
+        let baseMonths = 4;
+        if (moment(finalRetirementDate.toString(), 'YYYY-MM-DD').get('year') <= 2015) {
+            return baseYears + ' vite e' + baseMonths + ' muaj';
+        }
+        const monthsToAdd = (moment(finalRetirementDate.toString(), 'YYYY-MM-DD').get('year') - 2015) * 4;
+        baseYears += monthsToAdd / 12;
+        baseMonths += monthsToAdd % 12;
+        if (baseMonths === 12) {
+            baseYears += 1
+            baseMonths = 0
+        }
+        if(baseYears >= 40) {
+            return '40 vite';
+        }
+        return baseYears.toFixed(0) + ' vite e ' + baseMonths + ' muaj';
+    }, [finalRetirementDate])
+
+    return (
+        <div>
+            <div className="text-2xl text-blue-700 font-bold mb-10">Llogarit moshen e daljes ne pension</div>
+            <FormControl>
+                <FormLabel className="text-center" id="demo-row-radio-buttons-group-label">Gjinia</FormLabel>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                >
+                    <FormControlLabel value="female" checked={gender === 'female'} control={<Radio/>} label="Femër"
+                                      onClick={() => setGender("female")}/>
+                    <FormControlLabel value="male" checked={gender === 'male'} control={<Radio/>} label="Mashkull"
+                                      onClick={() => setGender("male")}/>
+                </RadioGroup>
+            </FormControl>
+            <div className="my-20">
+                <FormControl>
+                    <FormLabel className="text-center" id="demo-row-radio-buttons-group-label">Datëlindja</FormLabel>
+                    <Input id="date" color="primary" type="date" value={birthday}
+                           onChange={(e) => setBirthday(e.target.value)}/>
+                </FormControl>
+            </div>
+            <Button variant="outlined" onClick={onSubmit}>Llogarit</Button>
+            {displayResults && (
+                <Card className="mt-20 p-5">
+                    <div className="flex justify-center text-blue-700 font-semibold">Rezultati</div>
+                    <CardContent>
+                        <div>Del ne pension ne {finalRetirementDate}</div>
+                        <div>Mosha e daljes ne pension: {ageOfRetirement}</div>
+                        <div>Eksperienca e punes: {calculateWorkExperience}</div>
+                    </CardContent>
+                </Card>
+            )}
+        </div>
+    )
+}
+
+export default HomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,123 +1,12 @@
 'use client';
-import {
-    Button,
-    Card,
-    CardContent,
-    FormControl,
-    FormControlLabel,
-    FormLabel,
-    Input,
-    Radio,
-    RadioGroup
-} from '@mui/material';
-import {useMemo, useState} from "react";
-import moment from "moment";
 import "moment/locale/sq";
+import HomePage from "@/app/HomePage";
 
 export default function Home() {
-    const [gender, setGender] = useState("male");
-    const [birthday, setBirthday] = useState("");
-    const [displayResults, setDisplayResults] = useState(false);
-    const [finalRetirementYear, setFinalRetirementYear] = useState(0);
-    const [finalRetirementMonthIndex, setFinalRetirementMonthIndex] = useState(0);
-    const [finalRetirementMonth, setFinalRetirementMonth] = useState('');
-
-    const getFinalRetirementAge = useMemo(() => {
-        const retirementFullYear = moment({year: finalRetirementYear, month: finalRetirementMonthIndex});
-        const diffMonths = retirementFullYear.diff(moment(birthday), "months");
-        const years = Math.floor(diffMonths / 12);
-        const months = diffMonths % 12;
-        if (years >= 67) {
-            return "67 vjec";
-        }
-        return months === 0 ? years + " vjec" : years + " vjec e " + months + " muaj";
-    }, [finalRetirementMonth, finalRetirementYear])
-
-    const onSubmit = () => {
-        if (!birthday) {
-            return;
-        }
-        if (gender === 'male') {
-            calculateRetirement(65, 0, 1 / 12);
-        } else {
-            calculateRetirement(60, 2, 2 / 12);
-        }
-        setDisplayResults(true);
-    };
-
-    const calculateRetirement = (defaultRetirementAge = 65, defaultRetirementMonth = 0, retirementAgeIncrease = 1 / 12) => {
-        const birthDateObj = new Date(birthday);
-        const retirementYear = birthDateObj.getFullYear() + defaultRetirementAge;
-        let retirementMonth = birthDateObj.getMonth() + 1;
-        let retirementYearIncrease = 0;
-
-        if (retirementYear >= 2033) {
-            retirementYearIncrease = (retirementYear - 2033) * retirementAgeIncrease;
-            retirementMonth += Math.floor(retirementYearIncrease) * 12;
-            const remainingMonths = Math.round((retirementYearIncrease % 1) * 12);
-            retirementMonth += remainingMonths;
-        }
-
-        setFinalRetirementYear(retirementYear + Math.floor(retirementMonth / 12));
-        moment.locale('sq');
-        setFinalRetirementMonthIndex(retirementMonth % 12 + defaultRetirementMonth);
-        const month = moment().month(retirementMonth % 12 + defaultRetirementMonth).format('MMMM');
-        setFinalRetirementMonth(month);
-    };
-
-    const calculateOldness = (defaultRetirementAge = 35, defaultRetirementMonth = 4, retirementAgeIncrease = 4 / 12) => {
-        const birthDateObj = new Date(birthday);
-        const retirementYear = birthDateObj.getFullYear() + defaultRetirementAge;
-        let retirementMonth = birthDateObj.getMonth() + 1;
-        let retirementYearIncrease = 0;
-
-        if (retirementYear >= 2015) {
-            retirementYearIncrease = (retirementYear - 2015) * retirementAgeIncrease;
-            retirementMonth += Math.floor(retirementYearIncrease) * 12;
-            const remainingMonths = Math.round((retirementYearIncrease % 1) * 12);
-            retirementMonth += remainingMonths;
-        }
-
-        const yearsOfExperience = retirementYear + Math.floor(retirementMonth / 12) - birthDateObj.getFullYear();
-        moment.locale('sq');
-        const month = retirementMonth % 12 + defaultRetirementMonth;
-        return yearsOfExperience + " vjec e " + month + " muaj";
-    };
 
     return (
         <main className="flex flex-col items-center justify-between p-24">
-            <div className="text-2xl text-blue-700 font-bold mb-10">Llogarit moshen e daljes ne pension</div>
-            <FormControl>
-                <FormLabel className="text-center" id="demo-row-radio-buttons-group-label">Gjinia</FormLabel>
-                <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                >
-                    <FormControlLabel value="female" checked={gender === 'female'} control={<Radio/>} label="Femër"
-                                      onClick={() => setGender("female")}/>
-                    <FormControlLabel value="male" checked={gender === 'male'} control={<Radio/>} label="Mashkull"
-                                      onClick={() => setGender("male")}/>
-                </RadioGroup>
-            </FormControl>
-            <div className="my-20">
-                <FormControl>
-                    <FormLabel className="text-center" id="demo-row-radio-buttons-group-label">Datëlindja</FormLabel>
-                    <Input id="date" color="primary" type="date" value={birthday}
-                           onChange={(e) => setBirthday(e.target.value)}/>
-                </FormControl>
-            </div>
-            <Button variant="outlined" onClick={onSubmit}>Llogarit</Button>
-            {displayResults && (
-                <Card className="mt-20 p-5">
-                    <div className="flex justify-center text-blue-700 font-semibold">Rezultati</div>
-                    <CardContent>
-                        <div>Del ne pension ne {finalRetirementMonth} {finalRetirementYear}</div>
-                        <div>Mosha e daljes ne pension: {getFinalRetirementAge}</div>
-                        <div>Eksperienca e punes: {calculateOldness()}</div>
-                    </CardContent>
-                </Card>
-            )}
+            <HomePage />
         </main>
     );
 };
